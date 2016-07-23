63ee555cbd0b2db788a8787de8428d17df73079d
Noah Peeters <noah.peeters@icloud.com>
2016-07-23 22:21:10 +0200

    Added functions to basic map types

--- bots_lib/src/map.rs
+++ bots_lib/src/map.rs
@@ -3,6 +3,8 @@ use std::collections::HashMap;
 /// Default type to save coordinates.
 pub type Coordinate = i32;
 
+/// Default enum to save directions
+#[allow(missing_docs)]
 pub enum Direction {
     Up,
     Down,
@@ -34,20 +36,46 @@ impl Position {
     //pub fn add()
 }
 
+/// Default tile types for `Tiles`;
+#[allow(missing_docs)]
 pub enum TileType {
     Plain,
     Water,
     Rock
 }
 
+/// A tile in a map
 pub struct Tile {
-    pub tileType: TileType
+    /// The type of the tile
+    pub tile_type: TileType
 }
 
+struct Chunk {
+    tiles: HashMap<Position, Tile>
+}
+
+
+/// The map in a game
 pub struct Map {
+    chunk: HashMap<Position, Tile>
+
 
 }
 
-pub struct MapPart {
+impl Map {
+    pub fn new() -> Map {
+        Map {
+            chunk: HashMap::new()
+        }
+    }
+
+    fn getTile(&self, loc: Position) {
+        self.chunk.get(loc / 100).tiles.get(loc)
+    }
+}
+
+/// A part of a map that is visible
+pub struct MapSection {
+    /// A `HashMap` for the `Tile`s
     pub tiles: HashMap<Position, Tile>
 }
