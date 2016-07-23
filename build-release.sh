3541bd8976ec314f139d9bb1665ff1437465c457
Noah Peeters <noah.peeters@icloud.com>
2016-07-23 18:43:34 +0200

    Added release mode to build script

---
+++ build-release.sh
@@ -0,0 +1,11 @@
+cd client
+cargo build --release
+
+cd ../user1
+cargo build --release
+
+cd ../server
+cargo build --release
+
+cd ..
+cp -R user1/target/release/libuser1.dylib server/user/user1.dylib
